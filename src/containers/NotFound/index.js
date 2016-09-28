ff188d4cf7f291ccdf96182c93a4deee3abd3b58
Ben Hansen <bnhansn@gmail.com>
2016-09-27 20:21:08 -0400

    update 404

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router';
 
 const NotFound = () =>
-  <div>
-    <Helmet title="Not Found" />
-    <p>Page was not found</p>
+  <div style={{ margin: '2rem auto', textAlign: 'center' }}>
+    <Helmet title="404 | Not Found" />
+    <p>Page not found</p>
+    <p><Link to="/">Go to the home page →</Link></p>
   </div>;
 
 export default NotFound;
